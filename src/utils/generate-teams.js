4598c9d7d9232738c08b8d882bfef64db287783d
cdoolaeghe <celia.doolaeghe.partner@decathlon.com>
2019-10-09 14:15:51 +0200

    make two teams of one player

--- src/utils/generate-teams.js
+++ src/utils/generate-teams.js
@@ -8,6 +8,8 @@ export const generateTeams = (players) => {
   const team2 = [];
 
   // Put players into teams
+  team1.push(players[0]);
+  team2.push(players[1]);
 
   return { team1, team2 };
 };
