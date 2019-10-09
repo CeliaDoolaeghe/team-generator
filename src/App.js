ac586fff33fd7320131b6cee6e5eaefff5b1616a
cdoolaeghe <celia.doolaeghe.partner@decathlon.com>
2019-10-09 13:15:48 +0200

    display generated teams

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import sportsmen from './data/sportsmen';
 import SportsmenList from './components/SportsmenList';
+import { generateTeams } from './utils/generate-teams';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Team Generator</h1>
-      <div>
-        <SportsmenList sportsmen={sportsmen}/>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: {
+        team1: [],
+        team2: [],
+      }
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Team Generator</h1>
+        <div>
+          <SportsmenList sportsmen={sportsmen}/>
+          <button onClick={() => {
+            this.setState({ teams: generateTeams(sportsmen) });
+          }}>
+            Generate teams
+          </button>
+        </div>
+        <div>
+          <SportsmenList sportsmen={this.state.teams.team1}/>
+          <SportsmenList sportsmen={this.state.teams.team2}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 }
 
 export default App;
---
+++ src/utils/generate-teams.js
@@ -0,0 +1,6 @@
+export const generateTeams = (sportsmen) => {
+  return {
+    'team1': [sportsmen[0]],
+    'team2': [sportsmen[1]],
+  }
+};
