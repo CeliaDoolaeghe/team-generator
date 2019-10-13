b03a9ced8fb82aac2bf9716f0e86aa20a2215893
cdoolaeghe <celia.doolaeghe.partner@decathlon.com>
2019-10-13 14:33:31 +0200

    make number input label clickable

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -2,8 +2,10 @@ import React, { Fragment } from 'react';
 
 export default ({ onChange, isValid }) => (
   <Fragment>
-    <label>Number of players:</label>
-    <input id="number" onChange={e => onChange(e.target.value)}/>
+    <label>
+      Number of players:
+      <input id="number" onChange={e => onChange(e.target.value)}/>
+    </label>
     {!isValid && <span className="error">Please enter a valid number</span>}
   </Fragment>
 );
