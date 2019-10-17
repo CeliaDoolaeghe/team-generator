e0d7f4ce43e361155ccf887039207e05f55d9adb
cdoolaeghe <celia.doolaeghe.partner@decathlon.com>
2019-10-17 10:59:51 +0200

    add white background color to select

--- src/components/LevelSelect.js
+++ src/components/LevelSelect.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ value, onChange }) => (
   <label>
     Level of teams:
-    <select className='element-form' defaultValue={value} onChange={e => onChange(e.target.value)}>
+    <select className='element-form select' defaultValue={value} onChange={e => onChange(e.target.value)}>
       <option value="beginner">Beginner</option>
       <option value="expert">Expert</option>
       <option value="balanced">Balanced</option>
